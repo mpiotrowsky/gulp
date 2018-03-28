251cc751a0a9e9ddeb51fa94a2126a9536bc4611
Mateusz Piotrowski <mpiotrov@gmail.com>
2018-03-28 15:00:03 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,32 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
-// watch files for changes and reload
-gulp.task('serve', function() {
-  browserSync({
-    server: {
-      baseDir: 'src'
-    }
-  });
+var sassFiles = 'src/scss/**/*.scss',
+    cssDest = 'src/css/';
 
-  gulp.watch(['*.html', 'css/**/*.css', 'js/**/*.js'], {cwd: 'src'}, reload);
+//one time sass compilation
+
+gulp.task('styles', function () {
+    return gulp.src(sassFiles)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(cssDest));
+});
+
+// watch for sass changes
+
+gulp.task('styles:watch', function () {
+    gulp.watch(sassFiles, ['styles']);
+});
+
+// watch files for changes and reload browser
+
+gulp.task('serve', function () {
+    browserSync({
+        server: {
+            baseDir: 'src'
+        }
+    });
+    gulp.watch(['*.html', 'css/**/*.css', 'js/**/*.js'], { cwd: 'src' }, reload);
 });
